d261254f5e402e70f2f4ab7569cb573b9cfe93ad
Vincent Ha Vinh <vincent.havinh@hotmail.fr>
2018-10-24 17:11:33 +0200

    Début du TestAdmissionController
    (crée juste un ordinateur avec 2 processeurs à 2 coeurs.)

---
+++ fr/sorbonne_u/datacenter_etudiant/tests/TestAdmissionController.java
@@ -0,0 +1,111 @@
+package fr.sorbonne_u.datacenter_etudiant.tests;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import fr.sorbonne_u.components.cvm.AbstractCVM;
+import fr.sorbonne_u.datacenter.hardware.computers.Computer;
+import fr.sorbonne_u.datacenter.hardware.processors.Processor;
+import fr.sorbonne_u.datacenter.hardware.tests.ComputerMonitor;
+
+public class TestAdmissionController extends AbstractCVM {
+
+	public static final String	ComputerServicesInboundPortURI = "cs-ibp" ;
+	public static final String	ComputerStaticStateDataInboundPortURI = "css-dip" ;
+	public static final String	ComputerDynamicStateDataInboundPortURI = "cds-dip" ;
+
+	/** 	Computer monitor component.										*/
+	protected ComputerMonitor						cm ;
+	
+	
+	public TestAdmissionController() throws Exception {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	@Override
+	public void			deploy() throws Exception
+	{
+		Processor.DEBUG = true ;
+
+		// --------------------------------------------------------------------
+		// Create and deploy a computer component with its 2 processors and
+		// each with 2 cores.
+		// --------------------------------------------------------------------
+		String computerURI = "computer0" ;
+		int numberOfProcessors = 2 ;
+		int numberOfCores = 2 ;
+		Set<Integer> admissibleFrequencies = new HashSet<Integer>() ;
+		admissibleFrequencies.add(1500) ;	// Cores can run at 1,5 GHz
+		admissibleFrequencies.add(3000) ;	// and at 3 GHz
+		Map<Integer,Integer> processingPower = new HashMap<Integer,Integer>() ;
+		processingPower.put(1500, 1500000) ;	// 1,5 GHz executes 1,5 Mips
+		processingPower.put(3000, 3000000) ;	// 3 GHz executes 3 Mips
+		Computer c = new Computer(
+							computerURI,
+							admissibleFrequencies,
+							processingPower,  
+							1500,		// Test scenario 1, frequency = 1,5 GHz
+							// 3000,	// Test scenario 2, frequency = 3 GHz
+							1500,		// max frequency gap within a processor
+							numberOfProcessors,
+							numberOfCores,
+							ComputerServicesInboundPortURI,
+							ComputerStaticStateDataInboundPortURI,
+							ComputerDynamicStateDataInboundPortURI) ;
+		this.addDeployedComponent(c) ;
+		c.toggleLogging() ;
+		c.toggleTracing() ;
+		// --------------------------------------------------------------------
+		
+		
+		// --------------------------------------------------------------------
+		// Create the computer monitor component and connect its to ports
+		// with the computer component.
+		// --------------------------------------------------------------------
+		this.cm = new ComputerMonitor(computerURI,
+									 true,
+									 ComputerStaticStateDataInboundPortURI,
+									 ComputerDynamicStateDataInboundPortURI) ;
+		this.addDeployedComponent(this.cm) ;
+		// --------------------------------------------------------------------
+	
+		
+		// --------------------------------------------------------------------
+		// Create the Admission Controller component.
+		// Il faut lui passer le(s) ordinateur(s) existant(s).
+		// --------------------------------------------------------------------
+			//TODO
+		// --------------------------------------------------------------------
+		
+	
+		// complete the deployment at the component virtual machine level.
+		super.deploy();
+	}
+	
+	
+	/**
+	 * execute the test application.
+	 * 
+	 * @param args	command line arguments, disregarded here.
+	 */
+	public static void	main(String[] args)
+	{
+		// Uncomment next line to execute components in debug mode.
+		// AbstractCVM.toggleDebugMode() ;
+		try {
+			final TestAdmissionController tac = new TestAdmissionController();
+			tac.startStandardLifeCycle(10000L) ;
+			// Augment the time if you want to examine the traces after
+			// the execution of the program.
+			Thread.sleep(100000L) ;
+			// Exit from Java (closes all trace windows...).
+			System.exit(0) ;
+		} catch (Exception e) {
+			throw new RuntimeException(e) ;
+		}
+	}
+	
+}
